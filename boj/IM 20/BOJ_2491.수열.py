1679f34d52c1c3a0582868223588a67cfc8b4336
Linkim <kimchaelin13@gmail.com>
2020-11-18 09:37:01 +0900

    1117

--- boj/IM 20/BOJ_2491.수열.py
+++ boj/IM 20/BOJ_2491.수열.py
@@ -5,20 +5,85 @@ sys.stdin=open('input.txt','r')
 9
 1 2 2 4 4 5 7 7 2
 
-클때 작을때 함수 따로 만들어서 체크하고
-더 큰값을 리턴한다
-
+0번부터 보다가 만약에 작은게 나오면? 그전까지의 길이를 저장하고,
+작은것부터 다시 봄. 최종까지 갔을때 가장 긴 길이? 출력
 '''
-def increase(i):
-    res=0
-    if arr[i] <= arr[i+1]:
-        cnt+=1
+#
+# def increase(nums):
+#     start = 0
+#     move = 0
+#     end = 1
+#     longest = 0
+#     while True:
+#         # 종료 조건
+#         if (move == N-1) or (end == N - 1) or (start==N-1):
+#             break
+#         if nums[move] <= nums[end]:
+#             end += 1
+#             move += 1
+#         else:
+#             move = end
+#             start = end
+#             end = move + 1
+#
+#         if (end-start)+1>longest:
+#             longest=(end-start)+1
+#
+#     return longest
+#
+#
+# N=int(input())
+# nums=list(map(int,input().split()))
+# ans=0
+# if increase(nums) > increase(nums[::-1]):
+#     ans=increase(nums)
+# else:
+#     ans=increase(nums[::-1])
+# print(ans)
+#
+#
+#
 
 
-def decrease(i):
-    
+def increase(nums):
+    cnt = 1
+    MAX = 1
+    for i in range(1,N):
+        if nums[i-1] <= nums[i]:
+            cnt+=1
+        else:
+            if cnt>MAX:
+                MAX=cnt
+            cnt=1
+        if cnt>MAX:
+            MAX=cnt
+    return MAX
 N=int(input())
-arr=list(map(int,input().split()))
-for i in range(N):
-    increase(i)
-    decrease(i)
+nums=list(map(int,input().split()))
+ans=0
+if increase(nums) >= increase(nums[::-1]):
+    ans=increase(nums)
+else:
+    ans=increase(nums[::-1])
+print(ans)
+
+# def check(arr):
+#     global ans
+#     cnt=1
+#     for i in range(1,N):
+#         if arr[i-1] <= arr[i]:
+#             cnt+=1
+#         else:
+#             cnt=1
+#
+#         if cnt>ans:
+#             ans=cnt
+#
+#
+# if __name__=='__main__':
+#     N = int(input())
+#     arr = list(map(int, input().split()))
+#     ans=1
+#     check(arr)
+#     check(arr[::-1])
+#     print(ans)
--- boj/IM 20/달팽이거꾸로(ver2).py
+++ boj/IM 20/달팽이거꾸로(ver2).py
@@ -1,6 +1,18 @@
 import sys
 sys.stdin=open('input.txt','r')
+'''
+N=7일때 d는 8번 바뀐다
 
+d=0 1칸
+d=1 1칸
+d=2 2칸
+d=3 2칸
+d=0 3칸
+d=1 3칸
+d=2 4칸
+d=3 4칸
+d=0 4칸
+'''
 di=[0,-1,0,1]
 dj=[-1,0,1,0]
 
@@ -9,15 +21,40 @@ arr=[[0]*N for _ in range(N)]
 
 i=N//2
 j=N//2
-d=0
-num=1
-size=1
 
-while True:
+num=1
+size=N+1 #
+
+d = 0
+s = 1
+t = 1
+while num<=N*N:
     arr[i][j]=num
 
-    while size:
-        ni = i +di[d]
-        nj = j +dj[d]
+    #next num
+    t=1
+    while t<=s:
+        if num > N*N:
+            break
+        num += 1
+        # pos next num
+        ni = i + di[d]
+        nj = j + dj[d]
+        if 0<=ni<N and 0<=nj<N and arr[ni][nj]==0:
+            arr[ni][nj]=num
+            i=ni
+            j=nj
+            t+=1
 
+    #처음에 가야하는 스텝이 s다
+    #그리고 d==0, d==3일때마다 s+1를 한다
+    #그리고 조건에 t가 s와 같아질때까지, 가게함
+    #t는 1부터 시작해서 ~ 움직일때마다 +1을 해줄것
+    if d==1 or d==3:
+        s+=1
+    d=(d+1)%4
+
+
+for x in arr:
+    print(x)
 
